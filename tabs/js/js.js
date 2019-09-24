a7e3e717810b984a414b46a5eb37ec9fed7b9dcc
Alexey Zibarev <axeworkmail@gmail.com>
2019-09-24 14:12:54 +0300

    tabs: check tab in wrapper and refactoring

--- tabs/js/js.js
+++ tabs/js/js.js
@@ -1,29 +1,33 @@
 'use strict';
 
 (function () {
-  function searchClickTab(e) {
-    let targetTab = e.target.closest('.tabs-header__tab');
+  function clickTab(wrapper, tab) {
+    let tabDataTarget = tab.dataset.target;
 
-    if (!targetTab) return;
-
-    let dataTarget = targetTab.dataset.target;
-
-    this.querySelectorAll('.tabs-active')
-      .forEach(activeTabItem => {
-        activeTabItem.classList.remove('tabs-active');
+    wrapper.querySelectorAll('.tabs_active')
+      .forEach(activeItem => {
+        activeItem.classList.remove('tabs_active');
       });
 
-    targetTab.classList.add('tabs-active');
-    this.querySelector(`.${dataTarget}`).classList.add('tabs-active');
+    tab.classList.add('tabs_active');
+    wrapper.querySelector(`.${tabDataTarget}`).classList.add('tabs_active');
+  }
+
+
+  function searchTab(e) {
+    let wrapper = e.currentTarget;
+    let tab = e.target.closest('.tabs-header__tab');
+
+    if (!tab) return;
+    if (!wrapper.contains(tab)) return;
+
+    clickTab(wrapper, tab);
   }
 
 
   document.querySelectorAll('.tabs-wrapper')
     .forEach(tabComponent => {
-      tabComponent.addEventListener('click', searchClickTab);
-
-      tabComponent.querySelector('.tabs-header__tab').classList.add('tabs-active');
-      tabComponent.querySelector('.tabs-panel').classList.add('tabs-active');
+      tabComponent.addEventListener('click', searchTab);
     });
 
 }());
